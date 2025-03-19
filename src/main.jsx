dee43cbf867e9492796ca9fa721182bd4532de38
Chandan-kr-dev <chandanramgar@gmail.com>
2025-03-19 21:44:48 +0530

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ createRoot(document.getElementById("root")).render(
       }}
     >
       <ThemeProvider>
-        <Navbar/>
+        {/* <Navbar/> */}
         <App />
       </ThemeProvider>
       ,
